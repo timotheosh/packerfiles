29933c82a83bd2f3e61666aa008c885b6dab69ae
Tim Hawes <tim@selfdidactic.com>
2019-05-01 22:30:33 -0400

    Tested and refined wildfly docker image.

--- wildfly/build.sh
+++ wildfly/build.sh
@@ -14,6 +14,29 @@ case $ARCH in
     ;;
 esac
 
+if [[ -z ${JBOSS_USER} ]];then
+    echo -n "Enter username for Wildfly admin: "
+    read JBOSS_USER
+    while [[ -z $JBOSS_PASS ]];do
+        echo -n "Enter password: "
+        stty -echo
+        read pass1
+        stty echo
+        echo
+	echo -n "Re-enter password: "
+        stty -echo
+	read pass2
+        stty echo
+        echo
+	if [[ $pass1 = $pass2 ]];then
+            JBOSS_PASS=${pass1}
+            break
+        else
+            echo "Passwords do not match!"
+        fi
+    done
+fi
+
 packer build -var "architecture=${ARCHITECTURE}" \
              -var "jboss_user=${JBOSS_USER}" \
              -var "jboss_pass=${JBOSS_PASS}" \
---
+++ wildfly/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -d -p 8080:8080 -p 9990:9990 -v /home/thawes/programs/share/wildfly/deployments/:/opt/jboss/wildfly/standalone/deployments/ timotheosh/wildfly
