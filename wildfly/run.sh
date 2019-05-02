b021dd46be98b79a607ad10ceea6931ad4e4f4ff
Tim Hawes <trhawes@gmail.com>
2019-05-02 12:18:59 -0400

    Update run script to auto-restart

--- wildfly/run.sh
+++ wildfly/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 8080:8080 -p 9990:9990 -v /home/thawes/programs/share/wildfly/deployments/:/opt/jboss/wildfly/standalone/deployments/ timotheosh/wildfly
+docker run -d --restart unless-stopped -p 8080:8080 -p 9990:9990 -v /home/thawes/programs/share/wildfly/deployments/:/opt/jboss/wildfly/standalone/deployments/ timotheosh/wildfly
