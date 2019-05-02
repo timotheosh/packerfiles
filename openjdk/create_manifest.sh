526f73907f580ddfada918a081d87f3506cd8a63
Tim Hawes <tim@selfdidactic.com>
2019-05-02 13:53:56 -0400

    Add scripts for creating manifests.

---
+++ openjdk/create_manifest.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker manifest create timotheosh/openjdk8:latest timotheosh/openjdk8:amd64 timotheosh/openjdk8:arm
---
+++ wildfly/create_manifest.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker manifest create timotheosh/wildfly:latest timotheosh/wildfly:amd64 timotheosh/wildfly:arm
